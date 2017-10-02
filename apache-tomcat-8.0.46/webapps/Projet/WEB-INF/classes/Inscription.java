c2a7e114b3064fec37e5fa3b0aa32024f272231d
Alexis LAMBOIS <lamboisa@meleze01.iut-infobio.priv.univ-lille1.fr>
2017-10-02 16:29:21 +0200

    second commit

--- apache-tomcat-8.0.46/webapps/Projet/WEB-INF/classes/Inscription.java
+++ apache-tomcat-8.0.46/webapps/Projet/WEB-INF/classes/Inscription.java
@@ -17,13 +17,18 @@ public class Inscription extends HttpServlet {
 
 			// connexion a la base
 			con = DriverManager.getConnection("jdbc:postgresql://psqlserv/da2i","lamboisa","moi");
-			System.out.println(con.toString());
+			
 			// execution de la requete
 			Statement stmt = con.createStatement();
-			String query = "SELECT * FROM client";
+			
+			String query = "SELECT cno FROM client WHERE mail='" + req.getParameter("mail") + "'";
 			ResultSet rs = stmt.executeQuery(query);
-			if(rs.next()){
-				System.out.println(rs.getString(0));
+			
+			if(!rs.next()){
+				if( ( req.getParameter("mdp").equals(req.getParameter("mdp2")) ) && (req.getParameter("nom").matches("^[a-zA-Z0-9_]*$")) ){
+					query = "INSERT INTO client(nom,mail,mdp) VALUES('"+req.getParameter("nom")+"','"+req.getParameter("mail")+"','"+req.getParameter("mdp")+"')";
+					stmt.executeUpdate(query);
+				}
 			}
 			
 		}catch (Exception e) {
