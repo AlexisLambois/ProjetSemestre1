42db0d03943c24aab957be9f43aff55de5afb84d
Amelie NISSE <nissea@meleze02.iut-infobio.priv.univ-lille1.fr>
2017-10-06 11:41:16 +0200

    TestAmelie

--- ProjetSemestre1-master/apache-tomcat-8.0.46/webapps/Projet/html_css/JavaScriptTest.js
+++
@@ -1,7 +0,0 @@
-//display pour la visibilité
-/*var form1 = document.getElementById('form1');
-form1.onclick = function() {
-	form2.style.display = "none";
-}
-var form2 = document.getElementById('form2');*/
-alert('test');
--- apache-tomcat-8.0.46/webapps/Projet/WEB-INF/classes/main.java
+++
@@ -1,6 +0,0 @@
-public class main{
-	public static void main(String [] arg) {
-		String mail = "lambois16@gmailcom";
-		System.out.println(mail.matches("^[a-zA-Z0-9_]*@[a-z]*[.][a-z]*$"));
-	}
-}
