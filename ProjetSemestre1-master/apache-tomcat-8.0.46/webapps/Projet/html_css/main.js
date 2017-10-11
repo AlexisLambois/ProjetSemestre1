29ab34962019cc2e35da007ee4437481896b74e6
AlexisLambois <lambois16@gmail.com>
2017-10-11 11:54:46 +0200

    restor

--- ProjetSemestre1-master/apache-tomcat-8.0.46/webapps/Projet/html_css/main.js
+++
@@ -1,7 +0,0 @@
-
-function test(truc){
-	console.log(truc[0]);
-}
-function disco(){
-	window.location.href = "index.jsp";
-}
--- ProjetSemestre1-master/apache-tomcat-8.0.46/webapps/Projet/html_css/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$conn_string = "host=localhost port=5432 dbname=da2i user=lamboisa password=moi";
-	$dbconn4 = pg_connect($conn_string);
-	$result = pg_query($dbconn4, "SELECT * FROM client");
-	// connexion à une base de données nommée "test" sur l'hôte "mouton" avec un
-	// nom d'utilisateur et un mot de passe
-	echo $result
-?>
